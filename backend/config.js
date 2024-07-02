f9cca1e80c6a9469a23c91234774fe06eb459ce6
abijith r <93731985+geekeeeee@users.noreply.github.com>
2024-07-02 23:02:18 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-export const PORT = "https://flash-cards-app-one.vercel.app";
+export const PORT = 2000;
 
 export const mongoDBurl = 
 'mongodb+srv://root:maleniaBladeOfMiquella@flashcards.qrybouz.mongodb.net/?retryWrites=true&w=majority&appName=FlashCards'
