aa491914681cf12b68f3d581f96451b9758c3dcb
abijith r <93731985+geekeeeee@users.noreply.github.com>
2024-07-02 20:40:44 +0530

    Update index.js for vercel launch

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,13 @@ import cors from 'cors';
 const app  = express();
 app.use(bodyParser.json({ limit: '30mb', extended: true}));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true}));
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-1whq.vercel.app"],
+        methods: ["GET", "POST"],
+        credentials : true
+    }
+));
 
 
 app.get('/', (req, res) => {
