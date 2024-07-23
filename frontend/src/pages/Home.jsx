9a0a99c6eafbd63b79b6a2c3fa70aaf96d4736d6
abijith r <93731985+geekeeeee@users.noreply.github.com>
2024-07-23 22:02:35 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,8 +4,7 @@ import './general.css'
 
 const Home = () => {
   return (
-    
-    <div className="bg-hero h-screen text-center p-0 m-0">
+    <div className="bg-hero bg-no-repeat bg-cover min-w-screen min-h-screen text-center p-0 m-0">
       <Navbar/>
       <div className='absolute top-[200px] left-[100px] flex flex-col gap-0'>
         <div className="bg-txtt poppins-semibold text-white text-[60px] w-[490px] h-[200px] text-start p-0 m-0 bg-no-repeat">
